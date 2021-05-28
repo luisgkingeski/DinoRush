7ef43a3cdd4e1c34e701fdfa7aec720adf6f514a
luisgkingeski <44435379+luisgkingeski@users.noreply.github.com>
2021-05-27 22:50:10 -0300

    Initial ADS script

---
+++ Assets/Resources/Scripts/Services/ADS.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Advertisements;
+
+public class ADS : SingletonMonobehaviour<ADS>
+{
+    string gameId = "1234567";
+    string mySurfacingId = "rewardedVideo";
+    bool testMode = true;
+
+    // Initialize the Ads listener and service:
+    void Start()
+    {
+        Advertisement.Initialize(gameId, testMode);
+    }
+
+    public void ShowRewardedVideo()
+    {
+        // Check if UnityAds ready before calling Show method:
+        if (Advertisement.IsReady(mySurfacingId))
+        {
+            Advertisement.Show(mySurfacingId);
+        }
+        else
+        {
+            Debug.Log("Rewarded video is not ready at the moment! Please try again later!");
+        }
+    }
+
+   
+}
